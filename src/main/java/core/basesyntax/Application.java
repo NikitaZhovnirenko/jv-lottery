4e26f1ee4eb897b354db9b45605339784b5ad487
Nikita Zhovnirenko <ar1884ci@gmail.com>
2026-02-22 03:00:59 +0200

    Implemented CollorSupplier, Colors enum, Lottery class with required methods.

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -1,11 +1,10 @@
 package core.basesyntax;
-
 public class Application {
 
     public static void main(String[] args) {
-        int balls = 3;
+        final int NUMBER_OF_BALLS = 3;
         Lottery lottery = new Lottery();
-        for (int i = 0; i < balls; i++) {
+        for (int i = 0; i < NUMBER_OF_BALLS; i++) {
             System.out.println(lottery.getRandomBall());
         }
     }
